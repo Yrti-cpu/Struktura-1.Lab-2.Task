cc092ca497503e688c146fe53c36e5938f654436
Danila Dotsenko <i923b09@voenmeh.ru>
2023-04-03 00:25:12 +0300

    добавил конструктор копирования и перегрузил оператор =

--- 2.Task/Stack.cpp
+++ 2.Task/Stack.cpp
@@ -7,6 +7,17 @@ Stack::Stack(short capacity)
     arr = new short[capacity];
 }
 
+Stack::Stack(const Stack& obj)
+{
+    this->arr = new short[obj.capacity] {};
+    this->top = obj.top;
+    this->capacity = obj.capacity;
+    for (int i = 0; i <= obj.top; i++)
+    {
+        this->arr[i] = obj.arr[i];
+    }
+}
+
 Stack::~Stack()
 {
     delete[] arr;
@@ -53,4 +64,20 @@ int Stack::peek()
         return -1;
     }
     return arr[top];
+}
+
+Stack& Stack::operator=(const Stack& obj)
+{
+    if (this == &obj)
+        return *this;
+    this->top = obj.top;
+    this->capacity = obj.capacity;
+    delete[] this->arr;
+    this->arr = new short[obj.capacity] {0};
+    for (int i = 0; i <= obj.top; i++)
+    {
+        this->arr[i] = obj.arr[i];
+    }
+
+    return *this;
 }
--- 2.Task/Stack.h
+++ 2.Task/Stack.h
@@ -6,10 +6,12 @@ class Stack {
     short* arr;
 public:
     Stack(short capacity);
+    Stack(const Stack& obj);
     ~Stack();
     bool isEmpty();
     bool isFull();
     void push(short value);
     int pop();
     int peek();
+    Stack& operator =(const Stack& obj);
 };
