6eba771d6007b05ea19eb6bb7c5515da56ed5195
Danila Dotsenko <i923b09@voenmeh.ru>
2023-04-03 00:16:31 +0300

    перевод из постфиксной формы записи в инфиксную и подсчет результата

--- 2.Task/2.Task.cpp
+++ 2.Task/2.Task.cpp
@@ -5,6 +5,41 @@ int main() {
     using namespace std;
     string exp;
     cout << "Enter prefix expression: ";
-    cin >> exp;
+    getline(cin, exp);
+    Stack s(exp.size());
+    for (int i = exp.size() - 1; i >= 0; i--) {
+        if (isdigit(exp[i])) {
+            int num = 0;
+            while (i >= 0 && isdigit(exp[i])) {
+                num = num * 10 + (exp[i] - '0');
+                i--;  
+            }
+            i++;
+            s.push(num);
+           
+        }
+        else if (exp[i] == ' ') {
+            continue;
+        }
+        else {
+            int op1 = s.pop();
+            int op2 = s.pop();
+            switch (exp[i]) {
+            case '+':
+                s.push(op1 + op2);
+                break;
+            case '-':
+                s.push(op1 - op2);
+                break;
+            case '*':
+                s.push(op1 * op2);
+                break;
+            case '/':
+                s.push(op1 / op2);
+                break;
+            }
+        }
+    }
+    cout << "Result: " << s.pop() << endl;
     return 0;
 }
