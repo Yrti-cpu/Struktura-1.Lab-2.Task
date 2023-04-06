0625f51ddc34b592a4d97f9e01016173bbe8c516
Danila Dotsenko (laptop) <dadotsenko04@gmail.com>
2023-04-06 13:12:35 +0300

    добавил проверку входной строки

--- 2.Task/2.Task.cpp
+++ 2.Task/2.Task.cpp
@@ -1,13 +1,38 @@
 ﻿#include <string>
 #include "Stack.h"
 
+bool oper(std::string &exp) {//проверка на верную запись выражения
+    short i = 0;
+    unsigned char cnt = 0, cnt1 = 0;
+    for (i = 0; i < exp.size(); i++) {//идем по строке
+        if (exp[i] == '+' || exp[i] == '-' || exp[i] == '*' || exp[i] == '/') cnt++;
+        else if (exp[i] == ' ' || exp[i] == '\n' || exp[i] == '\t') continue;
+        else if (isdigit(exp[i]))
+        {
+            while (isdigit(exp[i]))
+            i++;
+            i--;
+            cnt1++;
+        }
+            
+    }
+    if ((cnt1 - cnt) == 1)
+        return true;
+   
+}
 int main() {
     using namespace std;
     string exp;
     cout << "Enter prefix expression: ";
-    getline(cin, exp);//считывание строки из потока 
+    while ( exp.size() < 3 || !oper(exp))//проверка ввода
+    {
+        system("CLS");
+        cout << "Enter prefix expression: ";
+        getline(cin, exp);//считывание строки из потока 
+
+    }
     Stack s(exp.size()); 
-    for (int i = exp.size() - 1; i >= 0; i--) {//проход по всей строке слева направо
+    for (short i = exp.size() - 1; i >= 0; i--) {//проход по всей строке слева направо
         if (isdigit(exp[i])) {//если цифра, то добавляем в стек
             int num = 0;
             string str = "";
@@ -18,7 +43,7 @@ int main() {
             reverse(str.begin(), str.end());//реверс строки
             num = stoi(str);//преобразование string в int
             i++;
-            s.push(num);//добавление в стек
+            s.push(num);
            
         }
         else if (exp[i] == ' ') {//пропуск пробелов
