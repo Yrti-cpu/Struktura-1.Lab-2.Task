3c0a55a8da142e73fc83abe7a04ab523e3ffca97
Danila Dotsenko <i923b09@voenmeh.ru>
2023-04-18 19:13:42 +0300

    проверка осуществляется во время обработки вводимомго выражения

--- 2.Task/2.Task.cpp
+++ 2.Task/2.Task.cpp
@@ -1,55 +1,44 @@
 ﻿#include <string>
 #include "Stack.h"
 
-bool oper(std::string &exp) {//проверка на верную запись выражения
-    short i = 0;
-    unsigned char cnt = 0, cnt1 = 0;
-    for (i = 0; i < exp.size(); i++) {//идем по строке
-        if (exp[i] == '+' || exp[i] == '-' || exp[i] == '*' || exp[i] == '/') cnt++;
-        else if (exp[i] == ' ' || exp[i] == '\n' || exp[i] == '\t') continue;
-        else if (isdigit(exp[i]))
-        {
-            while (isdigit(exp[i]))
-            i++;
-            i--;
-            cnt1++;
-        }
-            
-    }
-    if ((cnt1 - cnt) == 1)
-        return true;
-   
-}
+
 int main() {
     using namespace std;
-    string exp;
-    while ( exp.size() < 3 || !oper(exp))//проверка ввода
-    {
-        system("CLS");
-        cout << "Enter prefix expression: ";
-        getline(cin, exp);//считывание строки из потока 
-
-    }
+    unsigned char cnt = 0, cnt1 = 0;//счетчики операнд и операций 
+    string exp = "";
+    bool flag = true;
+    cout << "Enter prefix expression: ";
+    getline(cin, exp);//считывание строки из потока 
     Stack s(exp.size()); 
     for (short i = exp.size() - 1; i >= 0; i--) {//проход по всей строке слева направо
         if (isdigit(exp[i])) {//если цифра, то добавляем в стек
-            int num = 0;
-            string str = "";
-            while (isdigit(exp[i])) {//проверка на цифру
-                str += exp[i];//запись цифры в строку 
+            short num = 0, e = 1;
+            while (isdigit(exp[i])) {
+                num = num + (exp[i] - '0') * e;
+                e = e * 10;
                 i--;  
             }
-            reverse(str.begin(), str.end());//реверс строки
-            num = stoi(str);//преобразование string в int
+            
             i++;
             s.push(num);
+            cnt1++;
            
         }
         else if (exp[i] == ' ') {//пропуск пробелов
             continue;
         }
         else {//если оператор, то извлекаем из стека два операнда
-            int op1 = s.pop();
+            if (s.isEmpty())//предотвращение попытки извлечь что-то из пустого стека
+            {
+                flag = false;
+                break;
+            }
+            int op1 = s.pop(); 
+            if (s.isEmpty())
+            {
+                flag = false;
+                break;
+            }
             int op2 = s.pop();
             switch (exp[i]) {//применяем оператор и помещаем результат обратно в стек
             case '+':
@@ -65,8 +54,12 @@ int main() {
                 s.push(op1 / op2);
                 break;
             }
+            cnt++;
         }
     }
-    cout << "Result: " << s.pop() << endl;//в конце остается один элемент в стеке, который является результатом вычислений
+    if (flag && (cnt1 - cnt) == 1)//разность количества операндов и количества операций равна 1 и операции работали с элементами стека
+        cout << "Result: " << s.pop() << endl;//в конце остается один элемент в стеке, который является результатом вычислений
+    else
+        cout << "Incorrect expression" << endl;
     return 0;
 }
